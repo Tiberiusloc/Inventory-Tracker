656944cc0afab1819dc60fe48bee38d7642a729a
Tiberius Lockett <tlockett680@gmail.com>
2022-11-18 10:10:34 -0800

    Add newCoffeeForm to add coffee

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
+
+function NewCoffeeForm(props){
+  function handleNewCoffeeFormSubmission(event){
+    event.preventDefault();
+    props.onNewCoffeeCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      roast: event.target.roast.value,
+      pounds: event.target.pounds.value,
+      id: v4()
+    })
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm
+      formSubmissionHandler={handleNewCoffeeFormSubmission}
+      buttonText="Add New Coffee" />
+    </React.Fragment>
+  )
+}
+
+NewCoffeeForm.propTypes = {
+  onNewCoffeeCreation: PropTypes.func
+}
+
+export default NewCoffeeForm;
